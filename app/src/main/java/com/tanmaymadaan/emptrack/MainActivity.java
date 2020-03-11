377a9461ac4215145625d4a31182732446657951
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-03-11 12:08:53 +0530

    init

---
+++ app/src/main/java/com/tanmaymadaan/emptrack/MainActivity.java
@@ -0,0 +1,124 @@
+package com.tanmaymadaan.emptrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.karumi.dexter.BuildConfig;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start, stop;
+    TextView textView;
+
+    public LocationServiceGps locService;
+    public boolean mTracking = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.startTracking);
+        stop = findViewById(R.id.stopTracking);
+        textView =findViewById(R.id.textView);
+
+        final Intent intent = new Intent(this.getApplication(), LocationServiceGps.class);
+        this.getApplication().startService(intent);
+        this.getApplication().bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTracking = false;
+                locService.stopTracking();
+                toggleButtons();
+            }
+        });
+    }
+
+    private void startService() {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        locService.startTracking();
+                        mTracking = true;
+                        toggleButtons();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        if (response.isPermanentlyDenied()) {
+                            openSettings();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    private void toggleButtons() {
+        start.setEnabled(!mTracking);
+        stop.setEnabled(mTracking);
+        textView.setText( (mTracking) ? "Tracking" : "GPS Ready");
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null);
+        intent.setData(uri);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            String name = className.getClassName();
+            if (name.endsWith("LocationServiceGps")) {
+                locService = ((LocationServiceGps.LocationServiceBinder) service).getService();
+                start.setEnabled(true);
+                textView.setText("GPS Ready");
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            if (name.getClassName().equals("LocationServiceGps")) {
+                locService = null;
+            }
+        }
+    };
+
+
+}
